83a2b7448cedb2803348a67354ad3ffa6f8b6d1d
Ayushi Gaur <ayushigaur001@gmail.com>
2018-11-19 14:45:06 +0530

    first rendering list commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 class Counter extends Component {
   state = {
-    count: 1
+    count: 1,
+    tags: ["row1","row2","row3"]
   };
   render(){
      return(
        <React.Fragment>
          <span> { this.formatCount() } </span>
          <button> click </button>
+         <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
        </React.Fragment>
      );
   }
